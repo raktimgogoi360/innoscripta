74f4c8a88e1a7bea25c258fdb736d618073a3b0e
raktimgogoi360@ <raktimgogoi360@gmail.com>
2024-06-22 00:43:19 +0530

    phase 2 development

--- index.js
+++ index.js
@@ -6,14 +6,19 @@ const axios = require('axios');
 const querystring = require('querystring');
 const path = require('path');
 const bodyParser = require('body-parser');
-const crypto = require('crypto');
 const mongoAction = require('./lib/adapter');
-let createSubscription = require('./lib/createSubscription')
+const elasticOperation = require('./lib/ElasticSearch');
+
 const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(bodyParser.json());
 
+//handle error so that server doesnot crash
+app.use((err, req, res, next) => {
+    console.error('Error caught:', err);
+    res.redirect('/error');
+});
 
 //express to use public folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -36,7 +41,7 @@ app.get('/auth', (req, res) => {
     res.redirect(authUrl);
 });
 
-app.get('/auth/outlook/callback', async (req, res) => {
+app.get('/auth/outlook/callback', async (req, res, next) => {
     const code = req.query.code;
     const params = querystring.stringify({
         client_id: process.env.CLIENT_ID,
@@ -64,22 +69,18 @@ app.get('/auth/outlook/callback', async (req, res) => {
             "tenent_ID": process.env.TENANT_ID
         }
 
-        //fetch Subscription ID
-        let subscriptionID = await createSubscription(accessToken);
-
         let update = {
             "tenent_ID": process.env.TENANT_ID,
             "refresh_token": refresh_token,
-            "accessToken": accessToken,
-            "SubscriptionID": subscriptionID
+            "accessToken": accessToken
         }
-
+        await elasticOperation.updateDocument(collection ,where.tenent_ID, update , upsert = true)
         //mongo operation to update  auth details in DB
         await mongoAction.update(dbName, collection, where, update)
         console.log("**********Got aaccess_token from API ***************\n")
         res.redirect(`/emails?access_token=${accessToken}`);
     } catch (error) {
-        res.status(500).send(error.response.data);
+        next(error)
     }
 });
 app.get('/', (req, res) => {
@@ -88,102 +89,96 @@ app.get('/', (req, res) => {
 });
 
 //get emails
-app.get('/emails', (req, res) => {
+app.get('/emails', (req, res, next) => {
     const accessToken = req.query.access_token;
     if (!accessToken) {
         return res.status(401).send('Access token is missing');
     }
-    axios.get('https://graph.microsoft.com/v1.0/me', {
-        headers: {
-            Authorization: `Bearer ${accessToken}`
-        }
-    })
-        .then(async (response) => {
-            console.log("Got profile response")
-            let user = response.data || '';
-            let dbName = 'users';
-            let collection = 'UserDetails';
-            let where = {
-                "email": user.mail
-            };
-            let update = {
-                "user": user,
-                "id": user.id,
-                "email": user.mail
+    try {
+        axios.get('https://graph.microsoft.com/v1.0/me', {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
             }
-            await mongoAction.update(dbName, collection, where, update)
         })
+            .then(async (response) => {
+                console.log("Got profile response")
+                let user = response.data || '';
+                let dbName = 'users';
+                let collection = 'UserDetails';
+                let where = {
+                    "email": user.mail
+                };
+                let update = {
+                    "user": user,
+                    "id": user.id,
+                    "email": user.mail
+                }
+                await mongoAction.update(dbName, collection, where, update)
+            })
+            .catch((e) => {
+                console.log("Error in getting response, ", e);
+                next(e)
+            })
+    } catch (error) {
+        console.log("Error in getting response, ", error);
+        next(error)
+    }
 
     res.sendFile(path.join(__dirname, 'public', 'emailTable.html'));
 });
 
 
-app.get('/fetchMails', async (req, res) => {
+app.get('/fetchMails', async (req, res, next) => {
     let whereClause = { "tenent_ID": process.env.TENANT_ID };
     let dbName = 'users';
     let collection = "AuthDetails"
     let data = await mongoAction.fetch(dbName, collection, whereClause, {});
     var accessToken = data[0]["accessToken"];
-    //var access_token = dataFetch[0].accessToken;
-    axios.get('https://graph.microsoft.com/v1.0/me/messages', {
-        headers: {
-            Authorization: `Bearer ${accessToken}`
-        }
-    })
-        .then(async (response) => {
-            let mailData = response && response.data && response.data.value ? response.data.value : []
-            console.log("Printing mail Data" , mailData)
-            let dbName = "mailInfo";
-            let collection = "mails";
-            for (var i = 0; i < mailData.length; i++) {
-                let where = { "id": mailData[i].id };
-                let update = {
-                    "id": mailData[i].id,
-                    "subject": mailData[i].subject,
-                    "createdDateTime": mailData[i].createdDateTime,
-                    "lastModifiedDateTime": mailData[i].lastModifiedDateTime,
-                    "bodyPreview": mailData[i].bodyPreview,
-                    "isRead": mailData[i].isRead,
-                    "receivedDateTime": mailData[i].receivedDateTime,
-                    "from" : mailData[i].from.emailAddress.address,
-                    "name" : mailData[i].from.emailAddress.name
 
-
-                };
-                await mongoAction.update(dbName, collection, where, update)
+    try {
+        axios.get('https://graph.microsoft.com/v1.0/me/messages', {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
             }
-            var sort = { receivedDateTime: 1 }
-            const emails = await mongoAction.fetch(dbName, collection, {}, sort);
-           res.json(emails)
-          
         })
-        .catch(error => {
-            console.log("Got an error while fetching mails: ", error)
-            //res.status(500).send(error.response);
-            res.redirect('/')
-        });
+            .then(async (response) => {
+                let mailData = response && response.data && response.data.value ? response.data.value : []
+                let dbName = "mailInfo";
+                let collection = "mails";
+                for (var i = 0; i < mailData.length; i++) {
+                    let where = { "id": mailData[i].id };
+                    let update = {
+                        "id": mailData[i].id,
+                        "subject": mailData[i].subject,
+                        "createdDateTime": mailData[i].createdDateTime,
+                        "lastModifiedDateTime": mailData[i].lastModifiedDateTime,
+                        "bodyPreview": mailData[i].bodyPreview,
+                        "isRead": mailData[i].isRead,
+                        "receivedDateTime": mailData[i].receivedDateTime,
+                        "from": mailData[i].from.emailAddress.address,
+                        "name": mailData[i].from.emailAddress.name
 
+
+                    };
+                    await mongoAction.update(dbName, collection, where, update)
+                }
+                var sort = { receivedDateTime: 1 }
+                const emails = await mongoAction.fetch(dbName, collection, {}, sort);
+                res.json(emails)
+
+            })
+            .catch(error => {
+                console.log("Got an error while fetching mails: ", error)
+                next(error)
+            });
+    } catch (error) {
+        console.log("Got an error while fetching mails: ", error)
+        next(error)
+    }
 });
-
-
-app.post('/api/notifications', (req, res) => {
-    // Verify the clientState if you set it during subscription creation
-    if (req.body.value && req.body.value[0]) {
-        console.log('Notification received:', req.body);
-
-        // Process the notifications
-        req.body.value.forEach(notification => {
-            // Handle the notification (e.g., update your database, send an email, etc.)
-            console.log('Change detected:', notification);
-        });
-    }
-
-    // Respond to the validation token from Microsoft Graph
-    if (req.query && req.query.validationToken) {
-        res.status(200).send(req.query.validationToken);
-    } else {
-        res.status(202).end();
-    }
+// Route to handle errors
+app.get('/error', (req, res) => {
+    res.status(500).send('An error occurred while processing your request.');
 });
 
 app.listen(port, () => {
